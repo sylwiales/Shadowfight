f020d40c7706ffc76e35359ed45b11e54937ca00
HinoYoseii <slwlsnk@gmail.com>
2025-05-30 14:03:12 +0200

    lekka refaktoryzacja

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-javac -d out/production/gra_project src/*.java
+javac -d out/production/gra_project src/main/java/*.java
 java -cp out/production/gra_project Main
--- src/Fighter.java
+++
@@ -1,4 +0,0 @@
-public interface Fighter {
-    void attack(Fighter victim);
-    void takeHit(int damage, Data.AttackType attackType);
-}
